0f6139f19997daaa7ac820a9ba1da4c0fd9a01ae
Rosario Trotta <trottarosario@gmail.com>
2023-10-17 10:43:50 +0200

    commit

---
+++ com.protocollo.risparmio.brk/src/main/java/com/protocollo/risparmio/brk/ComProtocolloRisparmioBrkApplication.java
@@ -0,0 +1,12 @@
+package com.protocollo.risparmio.brk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComProtocolloRisparmioBrkApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ComProtocolloRisparmioBrkApplication.class, args);
+  }
+}
